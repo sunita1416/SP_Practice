a508439253bc8149cea359d9eee80f6beab72d89
Sunita <sunitapardeshi.nasik@gmail.com>
2023-03-14 19:53:50 +0000

    SP_Practice

---
+++ src/dailypractice/LowerCase.java
@@ -0,0 +1,15 @@
+package dailypractice;
+
+public class LowerCase {
+
+        public static void main(String[] args) {
+            String input1 = " THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.";
+            System.out.println(input1);
+            System.out.println(input1.toLowerCase());
+
+        }
+
+
+    }
+
+
